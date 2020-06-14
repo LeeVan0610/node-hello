918cc85ddf52f1d5db1f405916375184b85d3a85
LeeVan0610 <41725449+LeeVan0610@users.noreply.github.com>
2020-06-14 11:01:41 +0700

    update build

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node! Release Branch! test merge\n'
+  const msg = 'Hello Node! Release Branch!\n'
   res.end(msg);
 });
 
